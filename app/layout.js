d03cc6258ab1e82f25d554e0495dcdc2ec3b3b6c
NuhaG <nuhaghojaria@gmail.com>
2025-11-18 00:55:27 +0530

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: "Create Next App",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
         className={`antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
