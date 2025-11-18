48a1462e5d1b32e3610554dafbbb39cceaaecc93
Nuha Ghojaria <nuhaghojaria@gmail.com>
2025-11-18 13:50:03 +0530

    Update Header.jsx link to persona page

--- components/chat/Header.jsx
+++ components/chat/Header.jsx
@@ -7,8 +7,8 @@ export default function Header({ persona, toggleSidebar }) {
         <div className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-950 border-b border-gray-200 dark:border-gray-800 z-40 flex items-center px-6 py-2 justify-between">
             {/* left avatar det */}
             <div className="flex items-center gap-3">
-                {/* link to home */}
-                <Link href="/" className="p-1 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
+                {/* link to persona page */}
+                <Link href="/persona" className="p-1 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800">
                     <MdArrowBack className="w-6 h-6" />
                 </Link>
 
